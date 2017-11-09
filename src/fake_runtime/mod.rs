caeb8de0c787945c70397263591b63eb1cffaaf2
fro <frol.rage@gmail.com>
2017-11-09 18:56:09 +0300

    external test fail thread 'fake_runtime::test::test_external' panicked at 'already borrowed: BorrowMutError'

--- src/fake_runtime/mod.rs
+++ src/fake_runtime/mod.rs
@@ -5,18 +5,12 @@ use pwasm_std::hash::{Address, H256};
 use pwasm_std::bigint::U256;
 
 use super::External;
-use super::set_external;
 use super::Error;
-
-pub trait Endpoint {
-	fn dispatch(&mut self, payload: &[u8]) -> Vec<u8>;
-	fn dispatch_ctor(&mut self, payload: &[u8]);
-}
 
 // TODO: maybe extend to be enum Contract/External
 pub struct Account {
 	balance: U256,
-	endpoint: Box<Endpoint>,
+	endpoint: Box<Fn(&[u8]) -> Vec<u8>>,
 	storage: HashMap<H256, [u8; 32]>
 }
 
@@ -44,13 +38,13 @@ impl FakeRuntime {
 }
 
 impl External for FakeRuntime {
-	fn call(&mut self, address: &Address, val: U256, _input: &[u8], result: &mut [u8]) -> Result<(), Error> {
+	fn call(&mut self, address: &Address, val: U256, input: &[u8], result: &mut [u8]) -> Result<(), Error> {
 		self.call_stack = Some(Box::new(Call{
 			address: address.clone(),
 			value: val,
 			called_by: mem::replace(&mut self.call_stack, None)
 		}));
-		let res = self.accounts.get_mut(address).ok_or(Error)?.endpoint.dispatch(_input);
+		let res = (self.accounts.get_mut(address).ok_or(Error)?.endpoint)(input);
 		result.copy_from_slice(&res);
 		self.call_stack = match mem::replace(&mut self.call_stack, None) {
 			Some(call) => call.called_by,
@@ -58,37 +52,55 @@ impl External for FakeRuntime {
 		};
 		Ok(())
 	}
-	fn value(&mut self) {
-		self.call_stack.unwrap().value
+	fn value(&mut self) -> U256 {
+		match self.call_stack {
+			Some(ref stack) => stack.value.clone(),
+			None => 0.into()
+		}
 	}
 }
 
 #[cfg(test)]
 mod test {
 	use super::*;
+	use super::super::{set_external, EXTERNAL};
+	use pwasm_std::hash::{Address, H256};
+	use pwasm_std::ext;
 	#[test]
 	fn test_call() {
-		#[derive(Default)]
-		struct EndpointInst {
-		}
-		impl Endpoint for EndpointInst {
-			fn dispatch(&mut self, _payload: &[u8]) -> Vec<u8> {
-				vec![1u8]
-			}
-			fn dispatch_ctor(&mut self, _payload: &[u8]) {
-				unimplemented!()
-			}
-		}
+
 		let contract = Account{
 			balance: 0.into(),
-			endpoint: Box::new(EndpointInst::default()),
+			endpoint: Box::new(|payload| vec![1u8]),
 			storage: HashMap::new()
 		};
 		let mut accounts = HashMap::new();
 		accounts.insert("0x5484438c9bb11deeb87de29d7bf83c5d71dfd000".into(), contract);
+
 		let mut runtime = FakeRuntime::new("0x16a0772b17ae004e6645e0e95bf50ad69498a34e".into(), accounts);
 		let mut result = [0];
 		runtime.call(&"0x5484438c9bb11deeb87de29d7bf83c5d71dfd000".into(), 2.into(), &[0u8], &mut result);
 		assert_eq!(result, [1u8]);
+	}
+
+	#[test]
+	fn test_external() {
+		let contract = Account{
+			balance: 0.into(),
+			endpoint: Box::new(|payload| {
+				assert_eq!(ext::value(), 2.into());
+				vec![1u8]
+			}),
+			storage: HashMap::new()
+		};
+		let mut accounts = HashMap::new();
+		accounts.insert("0x5484438c9bb11deeb87de29d7bf83c5d71dfd000".into(), contract);
+
+		let runtime = set_external(Box::new(FakeRuntime::new("0x16a0772b17ae004e6645e0e95bf50ad69498a34e".into(), accounts)));
+		EXTERNAL.with(|r| {
+			let mut result = [0];
+			r.borrow_mut().call(&"0x5484438c9bb11deeb87de29d7bf83c5d71dfd000".into(), 2.into(), &[0u8], &mut result);
+			assert_eq!(result, [1u8]);
+		})
 	}
 }
