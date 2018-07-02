7d81f99585ea5f89c457d4d51e13d61d3559c5e0
Psilon <pzixel@gmail.com>
2018-07-02 18:56:20 +0300

    removes windows-only cfg

--- src/externs.rs
+++ src/externs.rs
@@ -259,21 +259,18 @@ pub unsafe extern "C" fn elog(topic_ptr: *const u8, topic_count: u32, data_ptr: 
 	});
 }
 
-#[cfg(windows)]
 #[doc(hidden)]
 #[no_mangle]
 pub unsafe extern "C" fn ret(_: *const u8, _: u32) -> ! {
 	unimplemented!()
 }
 
-#[cfg(windows)]
 #[doc(hidden)]
 #[no_mangle]
 pub unsafe extern "C" fn input_length() -> u32 {
 	unimplemented!()
 }
 
-#[cfg(windows)]
 #[doc(hidden)]
 #[no_mangle]
 pub unsafe extern "C" fn fetch_input(_: *mut u8) {
